511e50cdece73221ae9fc2408ff8e4e5046d8606
TaThuHa <thuhak2tltnpt@gmail.com>
2023-03-22 01:26:18 +0700

    up

---
+++ src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    private String name = "JOnh";
+    private String lop = "C02";
+
+    public Student() {
+    }
+
+    public String setName(String name) {
+        this.name = name;
+        return name;
+    }
+
+    public String setLop(String lop) {
+        this.lop = lop;
+        return lop;
+    }
+
+
+}
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println(student.setLop("java10"));
+        System.out.println(student.setName("ha"));
+
+    }
+}
